840c60a2797ae290a855df56c6f291d6a30d15cd
Zalika <zockleberry@newrelic.com>
2019-05-14 17:40:06 -0700

    creat module for plain HTML parser

--- gameofthronesScraper.js
+++ gameofthronesScraper.js
@@ -1,10 +1,9 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_episodes';
-
-
+const gameOfThronesParse = require('./gameOfThronesParse');
 rp(url)
-    .then(function(html){
+    .then(function(html) {
         //success
         const wikiUrls = [];
         //Traverse array as long as i is less than  155
@@ -12,13 +11,20 @@ rp(url)
             //Append(add) all elements that are labeled a inside of td that also have the href attribute to the array 
             wikiUrls.push($('td > a', html)[i].attribs.href);
         }
-        console.log(wikiUrls);
-        //Print the length of the elements with the tag 'a' inside of td in html
-        //console.log($('td > a', html).length);
-        //console.log($('td > a', html));
+        return Promise.all(
+            wikiUrls.map(function(url) {
+                return gameOfThronesParse('https://en.wikipedia.org'+ url);
+            })
+        );
+    })
+
+    .then(function(headlines){
+        console.log(headlines);
     })
     .catch(function(err){
         //handle error
+        //console.log(console.error();
+        
     });
 
     
